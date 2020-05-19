1c30ac38e9f4e5a0dee077913df096a9968f95f8
Paul Debevec <paulmdebevec@gmail.com>
2020-05-19 11:03:04 -0600

    Refactor SearchController's index method to adhere to rubocop line length

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,8 @@ class SearchController < ApplicationController
     @house = params[:house]
     key = ENV['HARRY_POTTER_API_KEY']
     conn = Faraday.new(url: 'https://www.potterapi.com')
-    response = conn.get("/v1/characters?key=#{key}&house=#{@house}&orderOfThePhoenix=true")
+    query_params = "?key=#{key}&house=#{@house}&orderOfThePhoenix=true"
+    response = conn.get("/v1/characters#{query_params}")
     @members = JSON.parse(response.body, symbolize_names: true)
   end
 end
