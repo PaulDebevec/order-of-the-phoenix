d0f1516cd1994495c69cefff96be2c742b4659c2
Paul Debevec <paulmdebevec@gmail.com>
2020-05-19 10:51:36 -0600

    Move test to appropriate directory

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome screen search' do
+  it 'When Gryffindor is selected and I search, I am redirected to /search ' do
+    visit '/'
+    click_button 'Search For Members'
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_content('There are 21 Members')
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role") # (if it exists)
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus") # (if it exists)
+    end
+  end
+end
